cba1eebdf214ec9885faba5e25470d7b3ba56fb4
inbar <inbar.bratspiess@wi-charge.com>
2025-02-11 15:22:08 +0200

    Added repl console to tusb console example Added a command to switch console from UART to TinyUSB CDC-ACM

--- examples/peripherals/usb/device/tusb_console/main/tusb_console_main.c
+++ examples/peripherals/usb/device/tusb_console/main/tusb_console_main.c
@@ -18,14 +18,81 @@
 #include "tinyusb.h"
 #include "tusb_cdc_acm.h"
 #include "tusb_console.h"
+#include "vfs_tinyusb.h"
 #include "sdkconfig.h"
+#include "esp_console.h"
+#include "linenoise/linenoise.h"
 
 static const char *TAG = "example";
+static esp_console_repl_t *repl = NULL;
+
+void set_dumb(void)
+{
+    linenoiseSetDumbMode(1);
+    return ;
+    
+    int probe_status = linenoiseProbe();
+    if (probe_status != linenoiseIsDumbMode()) {
+        linenoiseSetDumbMode(probe_status);
+        fprintf(stderr, "resetting dumb mode %d\n", probe_status);
+    }
+}
+
+static int do_switch_cli(int argc, char **argv)
+{
+    static bool cli_uart = true;
+
+    if (cli_uart) {
+        ESP_LOGI(TAG, "log -> UART");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        fprintf(stdout, "example: print -> stdout\n");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        fprintf(stderr, "example: print -> stderr\n");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        esp_tusb_init_console(TINYUSB_CDC_ACM_0); // log to usb
+        set_dumb();        
+        cli_uart = false;
+    } else {        
+        ESP_LOGI(TAG, "log -> USB");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        fprintf(stdout, "example: print -> stdout\n");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        fprintf(stderr, "example: print -> stderr\n");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        esp_tusb_deinit_console(TINYUSB_CDC_ACM_0); // log to uart
+        set_dumb();
+        cli_uart = false;
+    }
+
+    return 0;
+}
 
 void app_main(void)
 {
+    esp_err_t ret = ESP_OK;
+    esp_console_repl_config_t repl_config = {
+        .max_history_len = 32,
+        .history_save_path = NULL,
+        .task_stack_size = 4096,
+        .task_priority = 5,
+        .prompt = ">",
+        .max_cmdline_length = 1024
+    };
+
     /* Setting TinyUSB up */
     ESP_LOGI(TAG, "USB initialization");
+
+    esp_console_dev_uart_config_t hw_config = ESP_CONSOLE_DEV_UART_CONFIG_DEFAULT();
+    /* startthe UART as a*/
+    ret = esp_console_new_repl_uart(&hw_config, &repl_config, &repl);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "Failed initializing UART console");
+    } else {
+        ret = esp_console_start_repl(repl);
+        if (ret != ESP_OK) {
+            ESP_LOGE(TAG, "Failed initializing UART console");
+        }
+    }
 
     const tinyusb_config_t tusb_cfg = {
         .device_descriptor = NULL,
@@ -39,8 +106,28 @@ void app_main(void)
     tinyusb_config_cdcacm_t acm_cfg = { 0 }; // the configuration uses default values
     ESP_ERROR_CHECK(tusb_cdc_acm_init(&acm_cfg));
 
+    /* complete tusb-cdc console config */
+    esp_vfs_tusb_cdc_set_rx_line_endings(ESP_LINE_ENDINGS_CR);
+    esp_vfs_tusb_cdc_set_tx_line_endings(ESP_LINE_ENDINGS_CRLF);
+
     ESP_LOGI(TAG, "USB initialization DONE");
+
+    esp_console_cmd_t command = {
+        .command = "switch",
+        .help = "Switch from usb to uart cli",
+        .func = &do_switch_cli,
+        .argtable = NULL,
+    };
+
+    esp_console_cmd_register(&command);
+
+
     while (1) {
+        ESP_LOGI(TAG, "Keepalive");
+        vTaskDelay(10000 / portTICK_PERIOD_MS);
+    }
+
+    while (0) {
         ESP_LOGI(TAG, "log -> UART");
         vTaskDelay(1000 / portTICK_PERIOD_MS);
         fprintf(stdout, "example: print -> stdout\n");
